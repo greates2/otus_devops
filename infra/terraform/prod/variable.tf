aae7b718ca58a7d2549b8e5672eecef3a45f650d
Vadim Adelbaev <greates@yandex.ru>
2018-12-06 11:49:46 +0300

    add all hm

---
+++ infra/terraform/prod/variable.tf
@@ -0,0 +1,33 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+  default     = "~/.ssh/gcp.pub"
+}
+
+variable private_key_path {
+  description = "Path to the private key used to connect to instance"
+  default     = "~/.ssh/gcp"
+}
+
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
